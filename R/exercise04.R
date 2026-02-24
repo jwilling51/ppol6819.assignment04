1ad985d460070efeeb0ea10cdc4ea018e915bfae
swk2457 <sk2457@georgetown.edu>
2026-02-24 12:00:50 -0500

    Added random walk documentation

--- R/exercise04.R
+++ R/exercise04.R
@@ -6,6 +6,15 @@ set.seed(20260223)
 
 library(tidyverse)
 
+
+#' Random Walk Function
+#'
+#' @param nsteps A numeric value for the number of steps of the random walk
+#' @param stepsize A vector of two integers, one for the size of the forward step and one for the size of the backward step
+#' @param prob A vector of two decimal values for the probabilities associated with each step size
+#'
+#' @returns A tibble showing each step number and its location
+#'
 random_walk <- function(nsteps, stepsize, prob) {
 
   position <- vector(mode = "numeric", length = nsteps)
